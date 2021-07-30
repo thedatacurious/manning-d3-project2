1f23eed35d64d5cf4da9ba4e8b3e525b66c5fb1a
Alexandra <thedatacurious@gmail.com>
2021-07-31 01:09:19 +0800

    created correct scales for xaxis

--- js/gender-gap.js
+++ js/gender-gap.js
@@ -33,11 +33,11 @@ async function createViz(){
   .attr('width', dimensions.width)
   .attr('height', dimensions.height)
 
-  const histoChart = wrapper.append('g')
+  const distChart = wrapper.append('g')
   .style('transform',`translate(${dimensions.margin.left}px,${dimensions.margin.top}px)`)
 
 // Load data here
-const dataset = await d3.csv('./data/pay_by_gender_all.csv')
+ const dataset = await d3.csv('./data/pay_by_gender_all.csv')
 
 dataset.forEach(datum => {
     datum['earnings_USD_2019'] = +datum['earnings_USD_2019'].replace(",","").replace(",","")
@@ -52,6 +52,7 @@ dataset.forEach(datum => {
 
   sports.forEach(sport => {
     genders.forEach(gender => {
+
       const binsSet = {
         sport: sport,
         gender: gender,
@@ -61,6 +62,17 @@ dataset.forEach(datum => {
     });
   });
 
+  // for (let index = 0; index < sports.length; index++){
+  //   for (let indexj = 0; indexj < genders.length; indexj++){
+  //     const binsSet = await {
+  //       sport: sports[index],
+  //       gender: genders[indexj],
+  //       bins: d3.bin().thresholds(5)(dataset.filter(d => d.sport == sports[index] && d.gender == genders[indexj]).map(d => d.earnings_USD_2019)) // bin the earnings for a given sport-gender combination
+  //     };
+  //     bins.push(binsSet);
+  //   }
+  // }
+
   console.log(bins)
 
   //Generate scales
@@ -69,13 +81,23 @@ dataset.forEach(datum => {
   .domain([0,dataset.earnings_USD_2019])
   .range([dimensions.boundedHeight,0])
 
-  // const xScale = d3.scaleLinear()
-  // .domain([0, d3.max(bins.length)])
-  // .range()
+  const xScale = d3.scaleLinear()
+  .domain([0, d3.max(bins.map(d => d.bins.map(datum => datum.length)).flat())])
+  .range([0,60])
+
+  console.log(bins.map(d => d.bins.map(datum => datum.length)).flat())
 
   // Generate axes
 
-  const yAxisGenerator = d3.axisLeft().scale()
+  const yAxisGenerator = d3.axisLeft().scale(yScale)
+
+distChart.append('line')
+  .attr('x1',0)
+  .attr('y1',dimensions.boundedHeight)
+  .attr('x2',dimensions.boundedWidth)
+  .attr('y2',dimensions.boundedHeight)
+  .attr('stroke', 'black')
+  .attr('stroke-width',1)
 
 };
 
